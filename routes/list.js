69ca2bac56fdc91096141210f8d774eebff14815
abi-hong <flffjakfwm20@gmail.com>
2021-03-10 00:58:20 +0900

    리스트 클라이언트로 전송

--- routes/list.js
+++ routes/list.js
@@ -11,4 +11,14 @@ router.post("/save", (req, res) => {
     });
 });
 
+router.get("/getList", (req, res) => {
+    //리스트를 DB에서 가져와서 client로 보내기
+    List.find()
+        .populate('writer') //populate을 통해 User에 있는 모든 정보를 가져올 수 있음
+        .exec((err, lists) => {
+            if(err) return res.status(400).send(err);
+            res.status(200).json({ success: true, lists })
+        })
+});
+
 module.exports = router;
